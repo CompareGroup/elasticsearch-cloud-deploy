14e26df6d003a298b193b65deb221fdcee39a066
Ronald Konjer <ronaldkonjer@gmail.com>
2019-12-03 13:26:18 +0100

    Terraform upgrade

--- terraform-aws/vpc.tf
+++ terraform-aws/vpc.tf
@@ -1,13 +1,13 @@
 data "aws_vpc" "selected" {
-  id = "${var.vpc_id}"
+  id = var.vpc_id
 }
 
 data "aws_subnet_ids" "selected" {
-  vpc_id = "${var.vpc_id}"
+  vpc_id = var.vpc_id
 }
 
 resource "aws_security_group" "vpc-endpoint" {
-  vpc_id = "${var.vpc_id}" 
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 0
@@ -17,20 +17,28 @@ resource "aws_security_group" "vpc-endpoint" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-
 resource "aws_vpc_endpoint" "ec2" {
-  vpc_id = "${var.vpc_id}"
-  service_name = "com.amazonaws.${var.aws_region}.ec2"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ec2"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
-  security_group_ids = ["${aws_security_group.vpc-endpoint.id}"]
-  subnet_ids = ["${coalescelist(var.cluster_subnet_ids, data.aws_subnet_ids.selected.ids)}"]
+  security_group_ids = [aws_security_group.vpc-endpoint.id]
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibility in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  subnet_ids = [coalescelist(var.cluster_subnet_ids, data.aws_subnet_ids.selected.ids)]
 }
+
